994597f4b642ac5d83e82d94cc12b90ffc7bd40f
1F616EMO <root@1f616emo.xyz>
2024-08-23 21:20:30 +0800

    Revert "Stop hardcoding travelnet placeholder sounds"
    This reverts commit 23dbee4abb2d8a2ab287b560e5cdb05e7eeeae2c.

--- src/travelnet_register.lua
+++ src/travelnet_register.lua
@@ -95,7 +95,7 @@ function travelnet_redo.register_boxlike_travelnet(name, def)
             drop = "",
             is_ground_content = false,
             collision_box = placeholder_box,
-            sounds = def.sounds,
+            sounds = xcompat.sounds.node_sound_glass_defaults(),
         })
     end
 
